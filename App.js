e982d09f649a7d3aea77c4e41bc73244b9e99861
Lurie Kimmerle <LuraineKimmerle@gmail.com>
2024-03-02 14:18:13 +0800

    create OpeningScreen

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
 import { View, Text, Button } from 'react-native';
 import React from 'react';
 import { Link, Stack } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const OpeningScreen = ()=> {
+ return ( 
+ 
+  <SafeAreaView edges={['bottom'] style={{flex: 1}}}>
+
+      <Stack.Screen options={{title: 'BillSplitter'}}/>
+      <Link href="./camera" asChild>
+        <Button title="Go to Camera"/>
+      </Link>
+  </SafeAreaView>
+  
+  )
+
+}
